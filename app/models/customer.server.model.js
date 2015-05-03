ca1f7a9aaa6e856754d2dd7d199a0311cc8f4a16
saravanaraj.sathya@gmail.com <saravanaraj.sathya@gmail.com>
2015-05-03 14:13:59 -0700

    new changes

---
+++ app/models/customer.server.model.js
@@ -0,0 +1,62 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+/**
+ * Customer Schema
+ */
+var CustomerSchema = new Schema({
+	firstName: {
+		type: String,
+		default: '',
+		trim: true
+	},
+	lastName: {
+		type: String,
+		default: '',
+		trim: true
+	},
+	suburb: {
+		type: String,
+		default: '',
+		trim: true
+	},
+	country: {
+		type: String,
+		default: '',
+		trim: true
+	},
+	industry: {
+		type: String,
+		default: '',
+		trim: true
+	},
+	email: {
+		type: String,
+		default: '',
+		trim: true
+	},
+	phone: {
+		type: String,
+		default: '',
+		trim: true
+	},
+	referred: {
+		type: Boolean
+	},
+	created: {
+		type: Date,
+		default: Date.now
+	},
+	user: {
+		type: Schema.ObjectId,
+		ref: 'User'
+	}
+
+});
+
+mongoose.model('Customer', CustomerSchema);
